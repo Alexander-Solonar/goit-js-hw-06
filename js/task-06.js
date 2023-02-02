3dd7730a87a4296e12adec556bb9adbec66c2a64
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2023-02-02 22:02:06 +0200

    fix task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const textInput = document.querySelector("#validation-input");
 textInput.addEventListener("blur", (event) => {
   textInput.classList =
-    event.currentTarget.value.length >= textInput.dataset.length
+    event.currentTarget.value.length === Number(textInput.dataset.length)
       ? "valid"
-      : "invalid";
+      : "invalid";222222222222222222222
 });
