91780c65a29d35eb392e07b21aae2e0a13bb8b81
Alexander-Solonar <112549802+Alexander-Solonar@users.noreply.github.com>
2023-02-02 22:56:57 +0200

    Update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -2,9 +2,19 @@ const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", onSubmit);
 
-
 function onSubmit(event) {
   event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені!");
+  } else {
+    const user = {
+      email: email.value,
+      password: password.value,
+    };
+    event.currentTarget.reset();
+    console.log(user);
+  }
 }
-
-console.log(form);
